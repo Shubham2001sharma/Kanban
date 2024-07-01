e837ab96c3fa2949f467b99ecd11e1ed6af0d41e
shubham sharma <160488280+Shubham2001sharma@users.noreply.github.com>
2024-07-01 18:43:06 +0530

    Update Login.jsx

--- FRONTEND/src/Components/Login.jsx
+++ FRONTEND/src/Components/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
     }
 
     try {
-      const response = await axios.post("kanban-backend-kohl.vercel.app/login", {
+      const response = await axios.post("https://kanban-backend-kohl.vercel.app/login", {
         email,
         password,
       });
