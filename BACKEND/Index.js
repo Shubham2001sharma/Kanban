7981d7fffe037359708db39db962c81f63d6c63f
shubham sharma <160488280+Shubham2001sharma@users.noreply.github.com>
2024-07-01 16:42:45 +0530

    Update Index.js

--- BACKEND/Index.js
+++ BACKEND/Index.js
@@ -11,7 +11,7 @@ const secretkey = "hello";
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["kanban-dv6u.vercel.app"],
     credentials: true,
 }));
 app.use(express.json());
